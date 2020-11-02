e0fe6128ea2ab8e81625633ce6cb010ec458551f
shichuyu <1042048096@qq.com>
2020-11-02 21:02:36 +0800

    更新爬取数据，新增iconv-lite库，转码爬取中文乱码问题

--- crawl4.js
+++ crawl4.js
@@ -1,8 +1,6 @@
 // 奇趣测试网
 var http = require('http');
 var cheerio = require('cheerio');
-var fs = require("fs");//引入文件读写的模块
-const iconv = require('iconv-lite')
 var url = 'http://www.qqtest.com/s/9.html';
 var obj = [
   // {
@@ -19,51 +17,51 @@ http.get(url, (res) => {
   var html = '';
 
   res.on('data', (data) => {
-    // html += data;
-    html += iconv.decode(data, 'utf-8') // 解码，处理抓取的中文数据乱码
+    html += data;
   });
   res.on('end', () => {
-    const $ = cheerio.load(html,{decodeEntities: false});
+    console.log(document)
+    const $ = cheerio.load(html);
     let arr_name = [], arr_answer = [];
     let _contents = $('.test_contents');
     for (item in _contents) {
       if (item <= 40) {
         let qustion_content = $(_contents[item]).find('.fb').html().split('. ')[1]
         arr_name.push({
-          "seqId": 44,
-          "content": qustion_content,
-          "questionNo": 1,
-          "testAnswerDtos": [
-            {
-              "seqId": 175,
-              "questionId": 44,
-              "answerNo": "A",
-              "content": "是",
-              "contentDesc": ""
-            },
-            {
-              "seqId": 176,
-              "questionId": 44,
-              "answerNo": "B",
-              "content": "无法确定",
-              "contentDesc": ""
-            },
-            {
-              "seqId": 177,
-              "questionId": 44,
-              "answerNo": "C",
-              "content": "否",
-              "contentDesc": ""
-            }
-          ]
-        })
+            "seqId": 44,
+            "content": qustion_content,
+            "questionNo": 1,
+            "testAnswerDtos": [
+              {
+                "seqId": 175,
+                "questionId": 44,
+                "answerNo": "A",
+                "content": "是",
+                "contentDesc": ""
+              },
+              {
+                "seqId": 176,
+                "questionId": 44,
+                "answerNo": "B",
+                "content": "无法确定",
+                "contentDesc": ""
+              },
+              {
+                "seqId": 177,
+                "questionId": 44,
+                "answerNo": "C",
+                "content": "否",
+                "contentDesc": ""
+              }
+            ]
+          })
         var answer = $(_contents[item]).find('.i_mid');
         arr_answer.push(answer)
       }
     }
     let arr_answer_new = []
     let arr_answer_new2 = []
-    /* for (item in arr_answer) {
+    for (item in arr_answer) {
       for (item_i in arr_answer[item]) {
         if (item_i <= 2) {
           if (item_i == 1) {
@@ -76,15 +74,14 @@ http.get(url, (res) => {
       arr_answer_new2.push(arr_answer_new)
       arr_answer_new = [];
       // $(arr_answer[item]).text().split('、')[1];
-    } */
-    // console.log(arr_name, arr_answer)
-    writeFile(JSON.stringify(arr_name))
+    }
+    console.log(arr_name,arr_answer)
   });
 }).on('error', (e) => {
-  console.error('获取数据出错',e);
+  console.log('获取数据出错');
 });
 
 //写出文件
 function writeFile(params) {
-  fs.writeFileSync('抑郁自评量表（SDS）.json', params, { encoding: 'utf-8', mode: 438 /*=0666*/, flag: 'w' });
+  fs.writeFileSync('抑郁自评量表（SDS）.json', params, { encoding: 'utf8', mode: 438 /*=0666*/, flag: 'w' };);
 }
