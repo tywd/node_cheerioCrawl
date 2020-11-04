05d7659ce8bfd6fd50f265fc9530107ad57fe68a
shichuyu <1042048096@qq.com>
2020-11-04 13:53:32 +0800

    新增测试题抓取

--- crawl5.js
+++ crawl5.js
@@ -2,15 +2,16 @@
  * @Description: 
  * @Author: shichuyu
  * @Date: 2020-11-02 19:36:39
- * @LastEditTime: 2020-11-03 21:14:16
+ * @LastEditTime: 2020-11-04 11:32:03
  * @LastEditors: shichuyu
  */
 // 奇趣测试网
 var http = require('http');
 var cheerio = require('cheerio');
 var fs = require("fs");//引入文件读写的模块
-const iconv = require('iconv-lite')
-var url = 'http://m.ydl.com/ceshi/testing/2731?fromPath=eyJzdGFydF91cmwiOiIvY2VzaGkvc3RhcnQvMjczMSIsInRlc3RpbmdfdXJsIjoiIiwicmVwb3J0X3VybCI6IiJ9';
+const iconv = require('iconv-lite');
+const { copy } = require('superagent');
+var url = 'https://m.ydl.com/ceshi/testing/2731?fromPath=eyJzdGFydF91cmwiOiIvY2VzaGkvc3RhcnQvMjczMSIsInRlc3RpbmdfdXJsIjoiIiwicmVwb3J0X3VybCI6IiJ9';
 // 数据格式 
 // 提取网页的重要信息，我们可以定义数据格式：
 
@@ -24,43 +25,63 @@ http.get(url, (res) => {
   res.on('end', () => {
     const $ = cheerio.load(html, { decodeEntities: false });
     var fx1 = document.getElementsByClassName('df')[0].childNodes;
-var arrAnswer = []
-var arrContent = []
-var arrContent2 = []
-for(item in fx1){
-    var answer_item = []
-    if(item < 9){
-       var answer_content  = fx1[item].querySelectorAll('ul')[0];
-        arrContent.push(answer_content)
+    var arrQuestion = [],  // 存放所有问题加答案选项
+      arrAnswer = [],  // 存放最终答案数组
+      arrContent = []; // 存放所有答案的父级元素
+    for (item in fx1) {
+      if (item < 9) {
+        var question_name = fx1[item].querySelectorAll('.mb25')[0].innerText.split('.')[1].replace(/(^\s*)|(\s*$)/g, "");
+        // 先获得所有问题
+        arrQuestion.push({
+          "seqId": 44,
+          "content": question_name,
+          "questionNo": 1,
+          "createTime": "2020-08-05 11:16:30",
+          "testAnswerDtos": []
+        });
+        var answer_content = fx1[item].querySelectorAll('ul')[0];
+        arrContent.push(answer_content) // 获得所有答案选项的父级
+      }
     }
-}
 
-//         console.log(arrContent)
-var answer3 = [],answer4 = []
-// console.log(arrContent[0])
-for(item2 in arrContent){
-    let arr2 = arrContent[item2].querySelectorAll('li')
-    console.log(arr2)
-    answer4 = [] 
-    for (item3 in arr2) {
-        if (item3 <= 4) {
-//           let answer_str = arr2[item3].innerText.split('.')[1]
-          let answer_str = arr2[item3].innerText
-//             console.log(answer_str)
-          answer4.push({
+    // 遍历所有答案选项的父级
+    for (item in arrContent) {
+      // 去除单个父级的子级li
+      let arr2 = arrContent[item].querySelectorAll('li')
+      var answer_item = [] // 存放单个答案选项
+      // 遍历单个子级li下的所有选项
+      for (item2 in arr2) {
+        if (item2 <= 2) {
+          let answer_str = arr2[item2].innerText
+          // 获得所有答案
+          answer_item.push({
             "seqId": 175,
             "questionId": 44,
-            "answerNo": JSON.stringify(parseInt(item3) + 1),
+            "answerNo": JSON.stringify(parseInt(item2) + 1),
             "content": answer_str,
             "contentDesc": ""
           })
         }
       }
-    answer3.push(answer4)
-}
-// console.log(answer3)
-        
-        
+      arrAnswer.push(answer_item) // 获得所有答案选项数组
+      arrQuestion[item].testAnswerDtos = arrAnswer[item] // 将所有答案选项数组一一对应到问题arrQuestion
+    }
+    console.log(arrQuestion)
+    var q_data = {
+      "seqId": 0,
+      "name": "夫妻性格相容性测试",
+      "tipsTitle": "",
+      "price": "0",
+      "originalPrice": "0",
+      "visitNum": 0,
+      "elapsedTime": 0,
+      "questionNumber": 0,
+      "commentNum": 0,
+      "desc": "<p>我们的生命中来来往往的人那么多，究竟谁才是过客，而谁又是命中注定令自己沦陷的那一个Mr.Right？你是否也因为匆匆的行者而迷乱了渴望的双眼？还是一如既往站在原地等待TA的出现？想知道哪种爱人你遇到就<span style=\"white-space: normal;\">该</span>抓住不放吗？快来测测哪种男人最适合你？</p>",
+      "createTime": "2020-08-05 11:16:30",
+      "questions": arrQuestion
+    }
+    copy(q_data);
   });
 }).on('error', (e) => {
   console.error('获取数据出错', e);
